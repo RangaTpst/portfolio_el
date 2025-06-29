340001eb30fbd68a36b24c7024ac28308c8bb990
nicolas rouillé <nicolas.rouillelanoe@gmail.com>
2025-06-29 22:11:00 +0200

    analitics

--- app/views/partials/header.php
+++ app/views/partials/header.php
@@ -19,6 +19,16 @@
   <?php endif; ?>
   <script src="https://unpkg.com/lucide@latest"></script>
 
+  <!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-TCY5PKX7YL"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-TCY5PKX7YL');
+</script>
+
 </head>
 <body style="display: flex; flex-direction: column; min-height: 100vh; margin: 0;">
 
